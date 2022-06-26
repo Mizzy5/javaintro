a5012a4c18e1092c67cf4f79a50e09659d9ece73
mizzy5 <100967807+mizzy5@users.noreply.github.com>
2022-06-26 10:57:59 +0100

    first commit

---
+++ intro.js
@@ -0,0 +1,14 @@
+alert('My name is Mariam Adekunle');
+alert('I am 5.9ft tall');
+alert('I am from Nigeria')
+
+
+var name = ('Mariam Adekunle');
+var height = ('5.9ft');
+var country = ('Nigeria');
+
+
+
+console.log(name);
+console.log(height);
+console.log(country);
